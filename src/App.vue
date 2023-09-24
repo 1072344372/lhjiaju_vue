b75be475244efbcb43529697a8a3f6a75f09c37b
lh <1072344372@qq.com>
2023-09-25 00:09:45 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <template>
+<!--
+1. App.vue页面可以用于布局界面
+2. <router-view/>就是路由指令
+3．会把路由到的页面内容，展示到<router-view/>
+4.比如 url地址 http://localhost:8080/I那么路由的 path就是/
+
+-->
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
@@ -28,3 +35,5 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+<script setup>
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+// 1. index.js是用于配置路由
+// 2. path: '/'，路径﹑当访问 http://localhost:8080/就路由到 HomeView 组件
+// 3．就会把HomeView组件的内容，返回给<router-view/>
 const routes = [
   {
     path: '/',
@@ -10,9 +13,6 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
